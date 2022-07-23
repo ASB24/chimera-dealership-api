5d45e0ad2b9ff513953f08324ae2e5bb3386f5b5
ASB24 <alexserafini2001@gmail.com>
2022-07-23 18:07:06 -0400

    add createJson function and index method

--- app/Http/Controllers/API/CarController.php
+++ app/Http/Controllers/API/CarController.php
@@ -3,10 +3,31 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Car;
 use Illuminate\Http\Request;
 
 class CarController extends Controller
 {
+
+    public function createJson($car){
+        return [
+            'model' => $car->get('model'),
+            'brand' => $car->get('brand'),
+            'year' => $car->get('year'),
+            'price' => $car->get('price'),
+            'color' => $car->get('color'),
+            'traction' => $car->get('traction'),
+            'motor' => [
+                'type' => $car->get('type'),
+                'hp' => $car->get('hp'),
+                'turbo' => $car->get('turbo'),
+                'cylinders' => $car->get('cylinders'),
+                'motor_liters' => $car->get('motor_liters')
+            ],
+            'user_id' => $car->get('user_id')
+        ];
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +35,9 @@ class CarController extends Controller
      */
     public function index()
     {
-        //
+        $car = Car::all();
+
+        return response()->json(CarController::createJson($car));
     }
 
     /**
@@ -22,10 +45,10 @@ class CarController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
-    {
-        //
-    }
+    // public function create()
+    // {
+    //     //
+    // }
 
     /**
      * Store a newly created resource in storage.
