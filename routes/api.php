ba152e05f779248568c097df9f3f0a90879cf522
ASB24 <alexserafini2001@gmail.com>
2022-07-23 19:40:04 -0400

    add the endpoints for the CarController

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\UserController;
+use App\Http\Controllers\API\CarController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,4 +22,12 @@ Route::controller(UserController::class)->group(function(){
     Route::get('users/{id}','show');
     Route::put('users/{id}','update');
     Route::delete('users/{id}','destroy');
+});
+
+Route::controller(CarController::class)->group(function(){
+    Route::get('cars','index');
+    Route::post('cars','store');
+    Route::get('cars/{id}','show');
+    Route::put('cars/{id}','update');
+    Route::delete('cars/{id}','destroy');
 });
