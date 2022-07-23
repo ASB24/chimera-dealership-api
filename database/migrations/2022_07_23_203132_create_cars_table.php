7aba24652b70f861ed49c440fe41fb7b4bbb865b
ASB24 <alexserafini2001@gmail.com>
2022-07-23 16:37:21 -0400

    add user and car migrations
    Changed default string length to 191 to allow migration of users

---
+++ database/migrations/2022_07_23_203132_create_cars_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('model');
+            $table->string('brand');
+            $table->year('year');
+            $table->bigInteger('price');
+            $table->string('color');
+            $table->string('traction');
+            $table->string('type');
+            $table->integer('hp');
+            $table->boolean('turbo');
+            $table->integer('cylinders')->nullable();
+            $table->double('motor_liters')->nullable();
+            $table->integer('user_id');
+            $table->timestamp('registered_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cars');
+    }
+}
